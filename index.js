d30d902de9f0569460860a56e1ef2cd4ff90d1d8
Sylvain Bataille <etu41216@henallux.be>
2019-11-27 01:27:06 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const datasource = require('purobot-datasource');
+
+const app = express();
+app.use(bodyParser.json());
+
+(app);
+
+app.post('/', function (req, res) {
+    let message = '';
+    if(req.body.nlp.entities.wrestler){
+        //For the moment we are just taking one wrestler in param
+        let match = datasource.getMatchByWrestler(req.body.nlp.entities.wrestler[0].value);
+        message = parse(match);
+    }else{
+        let match = datasource.getRandomMatch();
+        message = parse(match);
+    }
+    res.send({
+        replies: [{
+            type: 'text',
+            content: message,
+        }],
+        conversation: {
+        }
+    })
+});
+
+
+const port = 5000;
+app.listen(port, function () {
+    console.log(`App is listening on port ${port}`);
+});
+
+function parse(match){
+    let message = `Here is a match for you !\n${match.match}\n${match.date}\n`;
+    if(match.other_data){
+        message+= `${match.other_data}\n`;
+    }
+    message += `\n${match.link_jp}\n`;
+    if(match.link_en){
+        message += `Available in english: ${match.link_en}`
+    }
+    return message;
+}
