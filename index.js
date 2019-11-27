e620bf90272a94c248ae8500adfd7d51aff86fbc
Sylvain Bataille <etu41216@henallux.be>
2019-11-27 02:23:42 +0100

    dyn port

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.post('/', function (req, res) {
 
 
 const port = 5000;
-app.listen(port, function () {
+app.listen(process.env.PORT || 5000 , function () {
     console.log(`App is listening on port ${port}`);
 });
 
