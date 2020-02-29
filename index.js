1a78db4b5ad47187c3180935cc55b253b969f093
Sylvain Bataille <etu41216@henallux.be>
2020-02-29 18:45:09 +0100

    refactor and add french response

--- index.js
+++ index.js
@@ -1,25 +1,49 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const datasource = require('purobot-datasource');
-//const datasource = require('../purobot-datasource/index.js')
+//const datasource = require('purobot-datasource');
+const en = require('./texts/en.json')
+const fr = require('./texts/fr.json')
+const datasource = require('../purobot-datasource/index.js')
 
 const app = express();
 app.use(bodyParser.json());
 
-(app);
-
 app.post('/matches', function (req, res) {
-    console.log('post')
-    str = JSON.stringify(req.body, null, 4); // (Optional) beautiful indented output.
-    console.log(str); let message = '';
+    str = JSON.stringify(req.body, null, 4);
+    //console.log(str);
+    let message = '';
     if (req.body.nlp.entities.wrestler) {
         //For the moment we are just taking one wrestler in param
         let match = datasource.getMatchByWrestler(req.body.nlp.entities.wrestler[0].value);
-        message = parse(match);
+        message = getMessage(match, req.body.nlp.language);
     } else {
         let match = datasource.getRandomMatch();
-        message = parse(match);
+        message = getMessage(match, req.body.nlp.language);
     }
+    sendMessage(res, message);
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, function () {
+    console.log(`App is listening on port ${PORT}`);
+});
+
+function getMessage(match, language) {
+    let texts = language === 'fr' ? fr : en;
+    let message = `${texts.presentMatch}\n${match.match}\n${match.date}\n`;
+    if (match.other_data) {
+        message += `${match.other_data}\n`;
+    }
+    message += `\n${match.link_jp}\n`;
+    if (match.link_en) {
+        message += `\n${texts.enAvailable} ${match.link_en}`
+    }
+    return message;
+}
+
+function sendMessage(res, message) {
     res.send({
         replies: [{
             type: 'text',
@@ -28,22 +52,4 @@ app.post('/matches', function (req, res) {
         conversation: {
         }
     })
-});
-
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, function () {
-    console.log(`App is listening on port ${PORT}`);
-});
-
-function parse(match) {
-    let message = `Here is a match for you !\n${match.match}\n${match.date}\n`;
-    if (match.other_data) {
-        message += `${match.other_data}\n`;
-    }
-    message += `\n${match.link_jp}\n`;
-    if (match.link_en) {
-        message += `Available in english: ${match.link_en}`
-    }
-    return message;
 }
